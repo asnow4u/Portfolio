f8ce8c9cbea6a58035635e56921a52b364a56da1
Andrew <asnow4u@gmail.com>
2024-01-24 20:55:58 -0600

    Skybox

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+           
+    }
+
+
+    public void RotateUpward() 
+    { 
+    
+    }
+
+    public void RotateDownward()
+    {
+
+    }
+
+    public void RotateLeftward()
+    {
+
+    }
+
+    public void RotateRightward()
+    {
+
+    }
+
+}
